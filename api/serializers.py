949c5c14bf429eafd8a7a5cb5691cc1d80704623
jordij <jordi.joan@gmail.com>
2016-05-13 14:42:57 +1200

    offsets for marker labels improved. url included in the api

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WagtailCompanyPage
-        fields = ('title', 'lat', 'lon', 'location', 'children_count')
+        fields = ('title', 'lat', 'lon', 'location', 'children_count', 'url')

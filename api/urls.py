a09a0becbd664813cc69b50072121ecd2eead4f7
jordij <jordi.joan@gmail.com>
2016-05-18 14:26:51 +1200

    map class

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,7 @@ router.register(r'companies', CompanyViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:

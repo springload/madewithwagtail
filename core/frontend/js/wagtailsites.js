b720763cc1992d15e74996206a7bf0ba4c32ad05
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-29 18:59:23 +0300

    Add recruitment message

--- core/frontend/js/wagtailsites.js
+++ core/frontend/js/wagtailsites.js
@@ -7,9 +7,20 @@ if ('ontouchstart' in window) {
     document.documentElement.className = document.documentElement.className + ' no-touch';
 }
 
+const message = `
+[ SPRINGLOAD ]
+Want to ship things that matter, at scale? Springloaders work on sites and apps with more than 54 million sessions/year.
+You could join our cross-functional dev team and work on challenging projects in Python, Node and ReactJS.
+Send your CV to apply@springload.co.nz, or better yet, send us a pull request: https://github.com/springload
+`;
+
 class Site {
     constructor() {
         GA.init();
+
+        if ('info' in console) {
+            console.info(message);
+        }
     }
 }
 

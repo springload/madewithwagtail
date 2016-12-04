c9a2271f978854c91413d8aec89974befcc978dc
Thibaud Colas <thibaudcolas@gmail.com>
2016-12-04 19:27:16 +1300

    Remove unused compilation code

--- gulpfile.js/svg.js
+++ gulpfile.js/svg.js
@@ -5,33 +5,13 @@ var svgmin = require('gulp-svgmin');
 var path = require( 'path');
 var size = require('gulp-size');
 var rename = require('gulp-rename');
-var inject = require('gulp-inject');
-
-
-gulp.task('svg:inline', function () {
-
-    return gulp
-        .src (path.join( config.paths.svg, "**", "*.svg" ) )
-        .pipe(rename({prefix: 'i-'}))
-        .pipe(svgmin())
-        .pipe(svgstore())
-        .pipe(size({ title: 'SVG', gzip: config.prod }))
-        .pipe(gulp.dest( path.join(config.paths.assets, "images") ));
-
-});
-
 
 gulp.task('svg', function () {
-    var svgs = gulp
+    return gulp
         .src( path.join( config.paths.svg, "**", "*.svg" ) )
         .pipe(rename({prefix: 'i-'}))
         .pipe(svgmin())
         .pipe(svgstore({ inlineSvg: true }))
         .pipe(rename("svg.html"))
         .pipe(gulp.dest( path.join( config.paths.templates, "core", "includes" ) ));
-
-    function fileContents (filePath, file) {
-        return file.contents.toString();
-    }
-
 });

120302584a22293678da5d05a1c59373b79da567
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-07 15:32:03 +0200

    Add ARIA attributes to facilitate tags toggle for screen readers

--- core/frontend/js/wagtailsites.js
+++ core/frontend/js/wagtailsites.js
@@ -25,16 +25,23 @@ class Site {
     }
 
     initTags() {
-        const tagsButton = document.querySelector('[data-tags-toggle]');
+        const tagsToggle = document.querySelector('[data-tags-toggle]');
         const tagsContainer = document.querySelector('[data-tags-additional]');
 
-        if (tagsButton) {
-            tagsButton.addEventListener('click', () => {
-                // First toggle hide/show in the inner element, then toggle the animation.
-                tagsContainer.children[0].classList.toggle('u-hide');
-                tagsContainer.classList.toggle('tags__additional--show')
-                tagsButton.innerHTML = tagsButton.innerHTML === 'Show more tags' ? 'Show fewer tags' : 'Show more tags';
-            });
+        const toggleTags = () => {
+            const wasVisible = JSON.parse(tagsToggle.getAttribute('aria-expanded'));
+            const isVisible = !wasVisible;
+            tagsToggle.setAttribute('aria-expanded', isVisible);
+            tagsContainer.setAttribute('aria-hidden', !isVisible);
+
+            // First toggle hide/show in the inner element, then toggle the animation.
+            tagsContainer.children[0].classList.toggle('u-hide', !isVisible);
+            tagsContainer.classList.toggle('tags__additional--show', isVisible)
+            tagsToggle.innerHTML = isVisible ? 'Show fewer tags' : 'Show more tags';
+        };
+
+        if (tagsToggle) {
+            tagsToggle.addEventListener('click', toggleTags);
         }
     }
 }

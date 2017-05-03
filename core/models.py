fab2005dc1c8a54830b25cd8f273f045eed42a05
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-03 16:38:30 +0300

    Fix encoding issue by replacing utf8 character with ASCII alternative

--- core/models.py
+++ core/models.py
@@ -359,7 +359,7 @@ class WagtailSitePage(WagtailPage):
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+',
-        help_text='Use the ratio 1200×996.',
+        help_text='Use the ratio 1200x996.',
     )
     image_tablet = models.ForeignKey(
         'wagtailimages.Image',

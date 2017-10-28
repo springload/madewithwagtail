2856e2249d98a1ee1d9d8f5ec1bb25f48d3c7494
Loic Teixeira <loic@springload.co.nz>
2017-10-28 17:25:56 +1000

    Add asterisk to required fields

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,19 @@
 from wagtail.wagtailforms.models import AbstractFormField, AbstractEmailForm
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailforms.forms import FormBuilder
 
 from modelcluster.fields import ParentalKey
 from wagtailcaptcha.models import WagtailCaptchaEmailForm
 
 from core.utilities import has_recaptcha
+
+
+class SubmitFormBuilder(FormBuilder):
+    def get_form_class(self):
+        form_class = super(SubmitFormBuilder, self).get_form_class()
+        form_class.required_css_class = 'required'
+        return form_class
 
 
 class SubmitFormField(AbstractFormField):
@@ -16,6 +24,9 @@ class SubmitFormPage(WagtailCaptchaEmailForm if has_recaptcha() else AbstractEma
     """
     Form page, inherits from WagtailCaptchaEmailForm if available, otherwise fallback to AbstractEmailForm
     """
+
+    form_builder = SubmitFormBuilder
+
     search_fields = []
     body = RichTextField(blank=True, help_text='Edit the content you want to see before the form.')
     thank_you_text = RichTextField(blank=True, help_text='Set the message users will see after submitting the form.')

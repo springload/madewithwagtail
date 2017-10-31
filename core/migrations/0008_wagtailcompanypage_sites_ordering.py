91a847cbd8670637420270489a6416134148c3e2
Loic Teixeira <hello@lta.me>
2017-10-31 16:06:28 +1000

    Allow sites order to be configured (#96)

---
+++ core/migrations/0008_wagtailcompanypage_sites_ordering.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_auto_20170503_2308'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='wagtailcompanypage',
+            name='sites_ordering',
+            field=models.CharField(default=b'created', max_length=20, choices=[(b'alphabetical', b'Alphabetical'), (b'created', b'Created'), (b'path', b'Path (i.e. manual)')], help_text=b'The order the sites will be listed on the page',),
+        ),
+    ]

8659d5bf4be7cec91c0ef1215874dd37093c4973
Jan Nakladal <mojeto1@gmail.com>
2018-01-18 15:30:29 +1300

    Adds automate submissions app

---
+++ submission/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.apps import AppConfig
+
+
+class SubmissionConfig(AppConfig):
+    name = 'submission'

122f09328d111b0b9bba7f8220b3f23478117765
Jan Nakladal <mojeto1@gmail.com>
2018-01-25 15:38:55 +1300

    Enables connection to postgresql DB from host

--- vagrant/postgres.sh
+++ vagrant/postgres.sh
@@ -7,3 +7,12 @@ su - vagrant -c "echo -n 'Changing postgres authorisation method ... ' && \
                              /etc/postgresql/9.3/main/pg_hba.conf && \
                  echo done! && \
                  sudo service postgresql restart"
+
+# Let us connect to the database from everywhere without credentials
+su - vagrant -c "echo -n 'Opening postgres to public ' && \
+                 sudo grep -q -F 'host	all	all	0.0.0.0/0	trust' /etc/postgresql/9.3/main/pg_hba.conf || \
+                            echo 'host	all	all	0.0.0.0/0	trust' | sudo tee -a /etc/postgresql/9.3/main/pg_hba.conf && \
+                 sudo sed -i \"s/.*#.*listen_addresses = 'localhost'/listen_addresses = '*'/g\" \
+                             /etc/postgresql/9.3/main/postgresql.conf && \
+                 echo done! && \
+                 sudo service postgresql restart"

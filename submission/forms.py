9b9deacdd0e23457843aba5e4ba0b1639166f747
Jan Nakladal <mojeto1@gmail.com>
2018-01-30 16:00:47 +1300

    Makes automate submission a two step process.

--- submission/forms.py
+++ submission/forms.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from captcha.fields import ReCaptchaField
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
@@ -16,34 +17,50 @@ from .utils import create_company_submission, get_collection_name, get_developer
 company_page_title_field = WagtailCompanyPage._meta.get_field('title')
 
 
-class SubmissionForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     """
-    A form that creates user, with privileges to edit developer page created for given developer user
+    A form that creates user
     """
-    company_name = company_page_title_field.formfield(
-        label='Developer name',
-        help_text="Who build the website? Provide a company name or your full name",
-        required=True
-    )
     if has_recaptcha():
         captcha = ReCaptchaField(label='')
 
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email', )
+        fields = UserCreationForm.Meta.fields + ('email',)
 
     def __init__(self, *args, **kwargs):
-        self.company_index_page = kwargs.pop('company_index_page', None) or get_developers_index_page()
-        super(SubmissionForm, self).__init__(*args, **kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs)
         # require email field
         self.fields['email'].required = True
         # TODO email verification?
         # TODO force unique email per user?
 
-    def clean_company_name(self):
+
+class SubmissionForm(forms.ModelForm):
+    """
+    A form that creates company page and grant privileges to edit that page to login user
+    """
+
+    class Meta(object):
+        fields = ('title', )
+        model = WagtailCompanyPage
+        labels = {
+            'title': 'Developer name',
+        }
+        help_texts = {
+            'title': 'Who build the website? Provide a company name or your full name',
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.company_index_page = kwargs.pop('company_index_page', None) or get_developers_index_page()
+        super(SubmissionForm, self).__init__(*args, **kwargs)
+        # require company title field
+        self.fields['title'].required = True
+
+    def clean_title_name(self):
         """
         Enforce company name is unique
         """
-        company_name = self.cleaned_data['company_name']
+        company_name = self.cleaned_data['title']
         group_name = get_permission_group_name(company_name)
         collection_name = get_collection_name(company_name)
         if self.company_index_page.has_company(company_name) or \
@@ -55,8 +72,6 @@ class SubmissionForm(UserCreationForm):
         return company_name
 
     @transaction.atomic()
-    def save(self, commit=True):
-        # create an user account
-        user = super(SubmissionForm, self).save(commit)
-        company_page = create_company_submission(user, self.cleaned_data['company_name'], self.company_index_page)
-        return user, company_page
+    def create_submission(self, user):
+        company_page = create_company_submission(user, self.cleaned_data['title'], self.company_index_page)
+        return company_page
--- submission/urls.py
+++ submission/urls.py
@@ -3,8 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from django.conf.urls import url
 
-from submission.views import AutomateSubmission
+from submission.views import AutomateSubmissionView, NewSubmissionView, SignUpView
 
 urlpatterns = [
-    url(r'^sign-up/$', AutomateSubmission.as_view()),
+    url(r'new/$', NewSubmissionView.as_view(), name='submission_new'),
+    url(r'^sign-up/$', SignUpView.as_view(), name='submission_signup'),
+    url(r'^developer/$', AutomateSubmissionView.as_view(), name='submission_developer'),
 ]

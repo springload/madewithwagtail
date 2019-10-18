42dc3616fe94070798a2c8d7360be7819dae9c11
Benjamin Wattie <2906911+b-jam@users.noreply.github.com>
2019-10-18 15:31:11 +1300

    Add dockerfiles and docker-compose so it can be run without vagrant

---
+++ docker/database/README.md
@@ -0,0 +1,6 @@
+# database
+
+The container runs PostgreSQL server.
+Any `.sql`, `.sql.gz` or `.sh` script in this folder will be executed on database container creation. 
+
+Ask DevOps team to provide `initdb.sql.gz` file and place it into `docker/database` directory
---
+++ docker/gunicorn.py
@@ -0,0 +1,16 @@
+accesslog = "-"
+errorlog = "-"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Forwarded-For}i)s"'
+capture_output = True
+forwarded_allow_ips = "*"
+secure_scheme_headers = {"X-CLOUDFRONT": "yes"}
+workers = 2
+worker_class = "gthread"
+worker_connections = 5
+bind = ":8000"
+keep_alive = 75
+chdir = "/madewithwagtail"
+
+# Obfuscate the Server header (to the md5sum of "Springload")
+import gunicorn
+gunicorn.SERVER_SOFTWARE = "04e96149a2f64d6135c82d199ab62122"

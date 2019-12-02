ac4d6ac066a24dcf90293816047a9afb80119c03
Benjamin Wattie <2906911+b-jam@users.noreply.github.com>
2019-12-02 15:36:50 +1300

    add s3 versioning

--- madewithwagtail/s3utils.py
+++ madewithwagtail/s3utils.py
@@ -1,8 +1,11 @@
 import mimetypes
 from storages.backends.s3boto3 import S3Boto3Storage
+from storages.utils import setting
 
 mimetypes.add_type("application/font-woff", "woff", strict=True)
 mimetypes.add_type("application/font-woff", "woff2", strict=True)
 
-StaticRootStorage = lambda: S3Boto3Storage(location="static")  # noqa: E731
+
+StaticRootStorage = lambda: S3Boto3Storage(  # noqa: E731
+    location=('static/%s' % setting('APPLICATION_VERSION', '')).strip("/"))
 MediaRootStorage = lambda: S3Boto3Storage(location="media")  # noqa: E731

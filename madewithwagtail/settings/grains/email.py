d7c4406c1200e2ce3bee8b487db089795022eb05
Eugene Dementiev <eugene@springload.co.nz>
2020-04-09 09:07:16 +1200

    Fix region for django_amazon_ses

--- madewithwagtail/settings/grains/email.py
+++ madewithwagtail/settings/grains/email.py
@@ -3,13 +3,13 @@ from .django import SITE_NAME
 
 EMAIL_BACKEND = "django_amazon_ses.EmailBackend"
 
-AWS_SES_REGION_NAME = "eu-west-1"
+AWS_SES_REGION = "ap-southeast-2"
 
 # Only for task failure notifications
 EXCEPTION_EMAIL_RECIPIENTS = ["tech-urgent@springload.co.nz"]
 
 # Default from address for CMS auto email messages (logs, errors..)
-SERVER_EMAIL = 'errors@madewithwagtail.org'
+SERVER_EMAIL = "errors@madewithwagtail.org"
 
 # Default from address for CMS email messages to users (forgot password etc..)
-DEFAULT_FROM_EMAIL = '%s@madewithwagtail.org' % SITE_NAME
+DEFAULT_FROM_EMAIL = "%s@madewithwagtail.org" % SITE_NAME

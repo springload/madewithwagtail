7f05c649fa38b9158f2852b82bb86d9e0c083075
Benjamin Wattie <ben.wattie@springload.co.nz>
2022-03-08 15:59:33 +1300

    django 2.2, wag 2.5

--- madewithwagtail/urls.py
+++ madewithwagtail/urls.py
@@ -3,7 +3,7 @@ import os
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from django.views.generic import TemplateView
 
 from api import urls as api_urls
@@ -12,7 +12,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.images import urls as wagtailimages_urls
-from wagtail.search import urls as wagtailsearch_urls
+
 from wagtail.search.signal_handlers import (
     register_signal_handlers as wagtailsearch_register_signal_handlers,
 )
@@ -23,15 +23,14 @@ admin.autodiscover()
 wagtailsearch_register_signal_handlers()
 
 urlpatterns = [
-    re_path(r"^django-admin/", admin.site.urls),
-    re_path(r"^admin/", include(wagtailadmin_urls)),
-    re_path(r"^search/", include(wagtailsearch_urls)),
-    re_path(r"^documents/", include(wagtaildocs_urls)),
-    re_path(r"^images/", include(wagtailimages_urls)),
-    re_path(r"^api/", include(api_urls)),
-    re_path(r"^sitemap\.xml$", sitemap),
-    re_path(
-        r"^robots\.txt$",
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("images/", include(wagtailimages_urls)),
+    path("api/", include(api_urls)),
+    path("sitemap.xml", sitemap),
+    path(
+        "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
 ]
@@ -39,7 +38,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns.append(re_path(r"^__debug__", include(debug_toolbar.urls)))
+    urlpatterns.append(path("__debug__", include(debug_toolbar.urls)))
 
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -58,5 +57,5 @@ else:
     )
 
 urlpatterns.append(
-    re_path(r"", include(wagtail_urls))
+    path("", include(wagtail_urls))
 )  # This must always be the last one since it's a catch all.

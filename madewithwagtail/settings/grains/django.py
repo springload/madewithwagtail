545e5a2acc4d59d7b556c8828d17e74f037dcd2c
katene30 <katenejack@gmail.com>
2022-03-21 12:21:34 +1300

    upgrade WT2.9.x > WT2.10.x

--- madewithwagtail/settings/grains/django.py
+++ madewithwagtail/settings/grains/django.py
@@ -53,7 +53,6 @@ MIDDLEWARE = (
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.cache.FetchFromCacheMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 )
 

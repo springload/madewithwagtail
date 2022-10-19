d95b64ded1cdb67bf80d5dae23cb6f7a31d43d03
Haydn Greatnews <haydn@springload.co.nz>
2022-10-20 11:04:34 +1300

    Convert a few more `url`s to `path`s

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
-from django.conf import settings
-from django.conf.urls import include, url
 from rest_framework import routers
+
+from django.conf import settings
+from django.urls import include, path, re_path
 
 from api.views import CompanyViewSet
 
@@ -8,16 +9,16 @@ router = routers.DefaultRouter()
 router.register(r"companies", CompanyViewSet)
 
 urlpatterns = [
-    url(r"^", include(router.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
     from django.views.generic import TemplateView
 
     urlpatterns += [
-        url(
-            "^data.json$",
+        path(
+            "data.json",
             TemplateView.as_view(
                 template_name="fixtures/data.json", content_type="text/json"
             ),
